f0040858031e8b7a3f30abf7a81a8a7dc2402f94
Aditya Punjani <aditya.punjani@flipkart.com>
2015-09-02 08:30:45 +0530

    fixes

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var fs = require('fs');
-var path = require('path');
-var compression = require('compression');
 
 var app = express();
 
-app.use(compression());
 app.use(express.static('public'));
 
+
+app.get('/sw.js', function(req, res){
+	res.set('Content-Type', 'application/javascript');
+	res.end(fs.readFileSync('sw.js'));
+});
 
 app.get('/home', function(req, res) {
 	res.end(fs.readFileSync('index.html', { encoding: 'utf8' }));
 });
 
 app.get('/', function(req, res) {
-	setTimeout(function(){
-		res.redirect('/home');
-	}, 5000);
+	res.redirect('/home');
 });
 
 var server = app.listen(process.env.PORT || 8080, function () {
